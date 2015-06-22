6429e97e8be5410c9565068b7d7e8163dd6103f4
unknown <blue20@RM02-20.fasfg.local>
2015-06-22 13:11:18 +0100

    Update

---
+++ Calculator/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        double firstTextBox = 0;
+        double secondTextBox = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //if ((int)e.KeyChar == 8) //for backspace
+            //    return;
+            //if (e.KeyChar == '.')
+            //    return;
+            //if (e.KeyChar < '0' || e.KeyChar > '9') //if the char entered is not 1-9
+            //    e.Handled = true; //don't display in the textbox
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((int)e.KeyChar == 8) //for backspace
+                return;
+            if (e.KeyChar == '.')
+                return;
+            if (e.KeyChar < '0' || e.KeyChar > '9') //if the char entered is not 1-9
+                e.Handled = true; //don't display in the textbox
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double result = 0;
+            firstTextBox = double.Parse(textBox1.Text);
+            secondTextBox = double.Parse(textBox2.Text); 
+            result = firstTextBox + secondTextBox;
+            textBox3.Text = result.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double result = 0;
+            firstTextBox = double.Parse(textBox1.Text);
+            secondTextBox = double.Parse(textBox2.Text);
+            result = firstTextBox - secondTextBox;
+            textBox3.Text = result.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double result = 0;
+            firstTextBox = double.Parse(textBox1.Text);
+            secondTextBox = double.Parse(textBox2.Text);
+            result = firstTextBox * secondTextBox;
+            textBox3.Text = result.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double result = 0;
+            firstTextBox = double.Parse(textBox1.Text);
+            secondTextBox = double.Parse(textBox2.Text);
+            result = firstTextBox / secondTextBox;
+            textBox3.Text = result.ToString();
+        }
+    }
+}
---
+++ CourseBookingApp/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CourseBookingApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e) //open file menu
+        {
+            OpenFileDialog theDialog = new OpenFileDialog(); //opens file selection window
+            theDialog.Title = "Open Text File";
+            theDialog.Filter = "TXT files|*.txt";
+            theDialog.InitialDirectory = @"C:\Users\blue20\Documents\vsprojects\Csharp-Basics\CourseBookingApp";
+
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                int j = 0;
+                string filename = theDialog.FileName;
+                string[] filelines = File.ReadAllLines(filename);
+                for (int i = 0; i < filelines.Length; i++) //loop through each line in the opened file
+                {
+                    //add file text to the listbox but skip every 3rd and 4th item
+                    if (j < 2)
+                        listBox1.Items.Add(filelines[i].Trim('"'));
+                    if (j == 3) //when we get to the 4th line in the file add a blank string to listbox and reset j to 0
+                    {
+                        listBox1.Items.Add("");
+                        j = 0;
+                    }
+                    else
+                        j++;
+                }
+            }
+        }
+        
+    }
+}
